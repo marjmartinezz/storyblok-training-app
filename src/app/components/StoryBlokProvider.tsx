69eca370b5ad2bd21a67fb195708f70ed2b7911c
Marj Martinez <marj@majordigital.com>
2024-10-10 17:29:52 +0100

    feat: add storyblok components

--- src/app/components/StoryBlokProvider.tsx
+++ src/app/components/StoryBlokProvider.tsx
@@ -1,14 +1,27 @@
 "use client";
-
 import type { PropsWithChildren } from "react";
 import { storyblokInit } from "@storyblok/react/rsc";
 import { Tour } from "./Tour";
+import { Page } from "./Page";
+import { Hero } from "./Hero";
+import { Grid } from "./Grid";
+import { Feature } from "./Feature";
+import { Testimonial } from "./Testimonial";
+import { RecommendedTours } from "./RecommendedTours";
 
 storyblokInit({
-  components: { tour: Tour },
+  components: {
+    tour: Tour,
+    page: Page,
+    hero: Hero,
+    grid: Grid,
+    feature: Feature,
+    testimonial: Testimonial,
+    recommended_tours: RecommendedTours,
+  },
   enableFallbackComponent: true,
 });
 
-export const StoryBlokProvider = ({ children }: PropsWithChildren) => {
-  return children;
+export const StoryblokProvider = ({ children }: PropsWithChildren) => {
+  return <>{children}</>;
 };
