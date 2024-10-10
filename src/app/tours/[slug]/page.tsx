5b22caea34ce112393c9176f4974860c52799420
Marj Martinez <marj@majordigital.com>
2024-10-10 17:28:55 +0100

    feat: Fetch Data from storyblok

--- src/app/tours/[slug]/page.tsx
+++ src/app/tours/[slug]/page.tsx
@@ -1,17 +1,32 @@
 import { getStoryblokApi, StoryblokStory } from "@storyblok/react/rsc";
+import { draftMode } from "next/headers";
+
+export const generateStaticParams = async () => {
+  const client = getStoryblokApi();
+  const response = await client.getStories({
+    content_type: "tour",
+    version: process.env.NODE_ENV === "development" ? "draft" : "published",
+  });
+
+  return response.data.stories.map((story) => ({ slug: story.slug }));
+};
 
 const fetchTourPage = async (slug: string) => {
+  const { isEnabled } = draftMode();
   const client = getStoryblokApi();
   const response = await client.getStory(`tours/${slug}`, {
-    version: "draft",
+    version:
+      process.env.NODE_ENV === "development" || isEnabled
+        ? "draft"
+        : "published",
   });
   return response.data.story;
 };
 
 const TourPage = async (props: any) => {
   const story = await fetchTourPage(props.params.slug);
-  // return <pre>{JSON.stringify(story, null, 2)}</pre>
-  return <StoryblokStory story={story} />
+  return <StoryblokStory story={story} />;
 };
 
 export default TourPage;
+ 
--- src/app/tours/page.tsx
+++ src/app/tours/page.tsx
@@ -1,6 +1,45 @@
-const TourPage = () => {
-  return <div>Tours Page</div>;
+import { getStoryblokApi, StoryblokStory } from "@storyblok/react/rsc";
+import { RecommendedTour } from "@/app/components/RecommendedTour";
+import { draftMode } from "next/headers";
+
+const fetchToursPage = async () => {
+  const { isEnabled } = draftMode();
+  const client = getStoryblokApi();
+  const response = await client.getStory(`tours`, {
+    version:
+      process.env.NODE_ENV === "development" || isEnabled
+        ? "draft"
+        : "published",
+  });
+  return response.data.story;
 };
 
-export default TourPage;
- 
+const fetchAllTours = async () => {
+  const { isEnabled } = draftMode();
+  const client = getStoryblokApi();
+  const response = await client.getStories({
+    content_type: "tour",
+    version:
+      process.env.NODE_ENV === "development" || isEnabled
+        ? "draft"
+        : "published",
+  });
+  return response.data.stories;
+};
+
+const ToursPage = async () => {
+  const story = await fetchToursPage();
+  const tours = await fetchAllTours();
+
+  return (
+    <div>
+      <StoryblokStory story={story} />
+      <div className="grid md:grid-cols-2 gap-8 container mx-auto px-4 w-full py-16">
+        {tours.map((tour) => (
+          <RecommendedTour story={tour} key={tour.content._uid} />
+        ))}
+      </div>
+    </div>
+  );
+};
+export default ToursPage;
